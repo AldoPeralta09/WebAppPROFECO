1cd14e58afc7869dddd4e2a527b71662921e1d1a
AldoPeralta09 <1714110453@utec-tgo.edu.mx>
2016-11-11 11:32:01 -0600

    cambiar formato

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
 # -*- coding: utf-8 -*-
 import web
 import json
-import csv
 
 data = []
-
-with open('200datos.json', 'r') as file:
-    data = json.load(file)
-
-file = open("datos.csv","w")
-
-for row in data["results"]:
-    file.write("{},{},{},{},{}\n".format(row["nombreComercial"],row["producto"],row["precio"],row["latitud"],row["longitud"]))
-
-file.close()
 
 render = web.template.render("views/")
 
@@ -23,12 +12,9 @@ urls = (
 
 class index:
     def GET(self, data_list):
-        data_list = []
-        with open("datos.csv","r") as filename:
-            datos = csv.reader(filename, delimiter = ",")
-            for row in datos:
-                data_list.append(row)
-        return render.index(data_list)
+        with open('200datos.json', 'r') as file:
+            data = json.load(file)
+        return render.index(data["results"])
     
 if __name__ == "__main__":
     app = web.application(urls, globals())
